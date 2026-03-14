f66f6ba73680167b3dc04c96f44bb947c66a217b
Alex Rock Ancelet <pierstoval@gmail.com>
2026-03-14 02:03:04 +0100

    -

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,11 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.setOutputDirectory("docs");
+    eleventyConfig.addPlugin(HtmlBasePlugin);
 };
+
+export const config = {
+    pathPrefix: "/gwttrpg/",
+}
