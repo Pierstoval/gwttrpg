f9d4a60e88c77dcbbb126c12f003682d827a780d
Alex Rock Ancelet <pierstoval@gmail.com>
2026-03-19 22:07:45 +0100

    -

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,11 @@
 import { HtmlBasePlugin } from "@11ty/eleventy";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.setOutputDirectory("docs");
     eleventyConfig.addPlugin(HtmlBasePlugin);
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 };
 
 export const config = {
